d053f6bf73f4179c5cc8850b6e536aa699081e47
Dmytro KUCHURA <dmytro.kuchura@raiffeisen.ua>
2023-11-06 17:07:07 +0200

    update ci

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, PUT, POST, DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Your-Cast API')
     .setDescription('The Your-Cast API for development')
